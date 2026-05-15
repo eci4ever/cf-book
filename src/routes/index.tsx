95b3f8da1ea7fde2faa5736270eafb0448bae8f4
eci4ever <eci4ever@gmail.com>
2026-05-15 17:52:42 +0800

    Initial

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { Button } from '#/components/ui/button'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+      <h1 className="text-4xl font-bold">Welcome to Book App</h1>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
+      <Button asChild size={'lg'} className="mt-6">
+        <Link to="/">
+          Go to Books
+        </Link>
+      </Button>
     </div>
   )
 }
